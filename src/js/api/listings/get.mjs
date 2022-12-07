e4fb104da930f5f0f3c2d3b36b3b0ab4bf04131b
Marianne Pedersen <mariannebpedersen@gmail.com>
2022-12-07 11:23:15 +0100

    Added sort order descending to url

--- src/js/api/listings/get.mjs
+++ src/js/api/listings/get.mjs
@@ -2,19 +2,19 @@ import { auctionBaseUrl } from "../constants.mjs";
 import { authFetch } from "../authFetch.mjs";
 
 const action = "/listings";
-const few = "?limit=3";
-const seller = "?_seller=true";
-const bids = "&_bids=true";
+const descOrder = "?sort=created&sortOrder=desc";
+const few = "&limit=3";
+const sellerAndBids = "?_seller=true&_bids=true";
 
 export async function getListings() {
-  const getListingsUrl = `${auctionBaseUrl}${action}`;
+  const getListingsUrl = `${auctionBaseUrl}${action}${descOrder}`;
   const response = await authFetch(getListingsUrl);
 
   return await response.json();
 }
 
 export async function getListingsFew() {
-  const getListingsUrl = `${auctionBaseUrl}${action}${few}`;
+  const getListingsUrl = `${auctionBaseUrl}${action}${descOrder}${few}`;
   const response = await authFetch(getListingsUrl);
 
   return await response.json();
@@ -25,7 +25,7 @@ export async function getListing(id) {
     throw new Error("A valid id is required");
   }
 
-  const getListingUrl = `${auctionBaseUrl}${action}/${id}${seller}${bids}`;
+  const getListingUrl = `${auctionBaseUrl}${action}/${id}${sellerAndBids}`;
   const response = await authFetch(getListingUrl);
 
   return await response.json();
