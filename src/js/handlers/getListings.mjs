61beae3951ecb774177a0be258175fc08ad93fa4
Marianne Pedersen <mariannebpedersen@gmail.com>
2022-12-07 22:35:18 +0100

    gathered exports-imports from templates

--- src/js/handlers/getListings.mjs
+++ src/js/handlers/getListings.mjs
@@ -1,27 +1,22 @@
-import {
-  renderListings,
-  renderListingsSearched,
-} from "../templates/listings.mjs";
-import { renderListingSpecific } from "../templates/listing-specific.mjs";
-import { renderListingsProfile } from "../templates/listings-profile.mjs";
+import * as temp from "../templates/index.mjs";
 import * as list from "../api/listings/get.mjs";
 
 export async function getListingsFeed() {
   const listings = await list.getListings();
   const container = document.querySelector("#allAuctions");
-  renderListings(listings, container);
+  temp.renderListings(listings, container);
 }
 
 export async function getListingsSearched() {
   const listings = await list.getListings();
   const container = document.querySelector("#allAuctions");
-  renderListingsSearched(listings, container);
+  temp.renderListingsSearched(listings, container);
 }
 
 export async function getListingsFeedHome() {
   const listings = await list.getListingsFew();
   const container = document.querySelector("#latestAuctions");
-  renderListings(listings, container);
+  temp.renderListings(listings, container);
 }
 
 export async function getListingSpecific() {
@@ -31,11 +26,11 @@ export async function getListingSpecific() {
 
   const listing = await list.getListing(id);
   const container = document.querySelector("#auctionSpecific");
-  renderListingSpecific(listing, container);
+  temp.renderListingSpecific(listing, container);
 }
 
 export async function getListingsProfile() {
   const listings = await list.getListingsByProfile();
   const container = document.querySelector("#usersListings");
-  renderListingsProfile(listings, container);
+  temp.renderListingsProfile(listings, container);
 }
