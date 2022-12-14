4c353dadfabc393b089069048f093fcec2b966ab
Marianne Pedersen <mariannebpedersen@gmail.com>
2022-12-14 20:00:29 +0100

    added create list date check

--- src/js/handlers/create-listing.mjs
+++ src/js/handlers/create-listing.mjs
@@ -3,6 +3,38 @@ import { createListing } from "../api/listings/create.mjs";
 /**
  * A listener for when users are creating a new listing
  */
+// export function setCreateListingListener() {
+//   const form = document.querySelector("#newListingForm");
+//   const newTitle = document.querySelector("#floatingTitle");
+//   const newDesc = document.querySelector("#floatingDescription");
+//   const newTags = document.querySelector("#floatingTags");
+//   const newMedia = document.querySelector("#floatingMedia");
+//   const newEndsAt = document.querySelector("#floatingEndsAt");
+
+//   if (form) {
+//     form.addEventListener("submit", async (event) => {
+//       event.preventDefault();
+
+//       const title = newTitle.value;
+//       const desc = newDesc.value;
+//       const tags = newTags.value.split(", ");
+//       const media = newMedia.value.split(", ");
+//       const endsAt = newEndsAt.value;
+
+//       await createListing({
+//         title: title,
+//         description: desc,
+//         tags: tags,
+//         media: media,
+//         endsAt: endsAt,
+//       });
+
+//       form.reset();
+//       location.reload();
+//     });
+//   }
+// }
+
 export function setCreateListingListener() {
   const form = document.querySelector("#newListingForm");
   const newTitle = document.querySelector("#floatingTitle");
@@ -20,17 +52,22 @@ export function setCreateListingListener() {
       const tags = newTags.value.split(", ");
       const media = newMedia.value.split(", ");
       const endsAt = newEndsAt.value;
+      const currentTime = new Date().toISOString();
 
-      await createListing({
-        title: title,
-        description: desc,
-        tags: tags,
-        media: media,
-        endsAt: endsAt,
-      });
-
-      form.reset();
-      location.reload();
+      console.log(currentTime);
+      if (endsAt > currentTime) {
+        await createListing({
+          title: title,
+          description: desc,
+          tags: tags,
+          media: media,
+          endsAt: endsAt,
+        });
+        form.reset();
+        location.reload();
+      } else {
+        alert("The date must be in the future");
+      }
     });
   }
 }
